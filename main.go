5a8e075354dd562c4719f59b66d28bc7af88e7ed
kakan <kakanghosh69@gmail.com>
2024-04-10 19:04:50 +0600

    init repo

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+func main() {
+	dataTypeExample()
+	// sortingExample()
+	// queueExample()
+	// stackExample()
+	// mapExample()
+	// setExample()
+	// priorityQueueExmple()
+	// customTypeExample()
+	// interfaceExample()
+	// concurrencyExample()
+}
+
+func customTypeExample() {
+	panic("unimplemented")
+}
+
+func interfaceExample() {
+	panic("unimplemented")
+}
+
+func dataTypeExample() {
+	// integer
+
+	// string
+
+	// array
+
+	// slice
+}
+
+func concurrencyExample() {
+	panic("unimplemented")
+}
+
+func sortingExample() {
+	panic("unimplemented")
+}
+
+func priorityQueueExmple() {
+	panic("unimplemented")
+}
+
+func setExample() {
+	panic("unimplemented")
+}
+
+func mapExample() {
+	panic("unimplemented")
+}
+
+func stackExample() {
+	panic("unimplemented")
+}
+
+func queueExample() {
+	panic("unimplemented")
+}
